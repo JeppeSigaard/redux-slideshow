52b6a03c7b0ad991040ce102d1e30c478a760193
Jeppe Sigaard <jeppe.sigaard@mobpro.com>
2017-12-15 12:17:59 +0100

    Better thunk demo, added arrow controls

--- app/components/keys.js
+++ app/components/keys.js
@@ -7,14 +7,27 @@ class Keys extends Component {
   constructor() {
     super();
     this.handleKey = this.handleKey.bind(this);
+    this.nextSlide = this.nextSlide.bind(this);
+    this.prevSlide = this.prevSlide.bind(this);
   }
 
   handleKey(e) {
-    if (e.keyCode === 39 && this.props.slide < this.props.max) {
-      this.props.setSlide(true);
+    if (e.keyCode === 39) {
+      this.nextSlide();
     }
 
-    if (e.keyCode === 37 && this.props.slide > 1) {
+    if (e.keyCode === 37) {
+      this.prevSlide();
+    }
+  }
+
+  nextSlide() {
+    if (this.props.slide < this.props.max) {
+      this.props.setSlide(true);
+    }
+  }
+  prevSlide() {
+    if (this.props.slide > 1) {
       this.props.setSlide(false);
     }
   }
@@ -28,7 +41,20 @@ class Keys extends Component {
   }
 
   render() {
-    return null;
+    return (
+      <div className="keys">
+        <div className="prev-slide" onClick={this.prevSlide}>
+          <svg id="icon-chevron-left" viewBox="0 0 32 32">
+            <path d="M19.219 7.448c-1.040-1.041-2.731-1.041-3.771 0l-8.553 8.552 8.553 8.552c0.52 0.521 1.203 0.781 1.885 0.781s1.365-0.26 1.885-0.781c1.041-1.041 1.041-2.729 0-3.771l-4.78-4.781 4.78-4.781c1.041-1.041 1.041-2.729 0-3.771z" />
+          </svg>
+        </div>
+        <div className="next-slide" onClick={this.nextSlide}>
+          <svg id="icon-chevron-right" viewBox="0 0 32 32">
+            <path d="M11.448 7.448c-1.041 1.041-1.041 2.729 0 3.771l4.78 4.781-4.78 4.781c-1.041 1.041-1.041 2.729 0 3.771 0.52 0.521 1.203 0.781 1.885 0.781s1.365-0.26 1.885-0.781l8.553-8.552-8.553-8.552c-1.040-1.041-2.731-1.041-3.771 0z" />
+          </svg>
+        </div>
+      </div>
+    );
   }
 }
 
--- app/slides/thunk-demo.js
+++ app/slides/thunk-demo.js
@@ -9,11 +9,16 @@ class Slide extends Component {
   }
 
   onClick() {
-    this.props.doAsync();
+    if (this.props.async.state !== "pending") {
+      this.props.doAsync();
+    }
   }
 
   render() {
     const { slide, async } = this.props;
+    let btnClass = "async-btn";
+    if (async.state === "pending") btnClass += " disabled";
+
     return (
       <div className="slide-content">
         {slide === 14 && (
@@ -31,9 +36,15 @@ class Slide extends Component {
         {slide === 15 && (
           <div>
             <h1>redux-thunk</h1>
+            <p>
+              Allows multiple, asynchronous dispatches, with access to
+              intermediate state changes.
+            </p>
             <div className="async-box">
-              <a href="#" onClick={this.onClick} className="async-btn">
-                Do a redux-thunk
+              <a href="#" onClick={this.onClick} className={btnClass}>
+                {async.state === "unstarted" && "Do a redux-thunk"}
+                {async.state === "pending" && "Thunking..."}
+                {async.state === "complete" && "Re-do a redux-thunk"}
               </a>
               <div className="black-box" style={{ width: "600px" }}>
                 <pre>Status: {async.state}</pre>
